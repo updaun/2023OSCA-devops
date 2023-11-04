4a52d36ae9bcab89cb4c54472b63fb53e104c311
updaun <updauney@daum.net>
2023-11-04 16:29:01 +0900

    ci

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,4 +12,4 @@ def test_hello_world(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.status_code != 500
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"hi": "World"}
